a977951e8ab942889855e61fd1c27eb63f76b5c1
Vitor Campos <vitorcampos742@gmail.com>
2021-11-24 19:01:38 -0300

    requisitos 5 e 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = { child: 0, adult: 0, senior: 0 };
+  obj.child = entrants.filter((item) => item.age < 18).length;
+  obj.adult = entrants.filter((item) => item.age >= 18 && item.age < 50).length;
+  obj.senior = entrants.filter((item) => item.age >= 50).length;
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  console.log(entrants);
+  const a = countEntrants(entrants);
+  return a.child * prices.child + a.adult * prices.adult + a.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const a = {};
+    species.forEach((item) => {
+      a[item.name] = item.residents.length;
+    });
+    return a;
+  }
+  let a = species.filter((item) => item.name === animal.specie)[0].residents;
+  if (animal.sex === 'female') {
+    a = a.filter((item) => item.sex === 'female');
+  } else if (animal.sex === 'male') {
+    a = a.filter((item) => item.sex === 'male');
+  }
+  return a.length;
 }
 
 module.exports = countAnimals;
