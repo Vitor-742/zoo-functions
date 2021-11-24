20b3e9f8d9a7f47f664cacad4ec328077733158e
Vitor Campos <vitorcampos742@gmail.com>
2021-11-24 15:29:28 -0300

    requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species
+    .find((item) => item.name === animal)
+    .residents.every((bicho) => bicho.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu códi
+function getSpeciesByIds(...ids) {
+  return ids.reduce((acc, id) => acc.concat(species.find((item) => id === item.id)), []);
 }
 
 module.exports = getSpeciesByIds;
