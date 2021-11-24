779d14ceeaadc81d4e26f2aeac2870c263a664ac
Vitor Campos <vitorcampos742@gmail.com>
2021-11-24 16:46:47 -0300

    requisitos 3 e 4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let nome = employees.find(
+    (item) => item.firstName === employeeName || item.lastName === employeeName,
+  );
+  if (nome === undefined) nome = {};
+  return nome;
 }
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((pessoa) => pessoa.managers.some((man) => man === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const a = employees.filter((pessoa) => pessoa.managers.some((item) => item === managerId));
+  return a.map((item) => `${item.firstName} ${item.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
