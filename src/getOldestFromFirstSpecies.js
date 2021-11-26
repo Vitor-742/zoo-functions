c15cd0b88eabc03fc20359708b707bacebbb6e96
Vitor Campos <vitorcampos742@gmail.com>
2021-11-26 15:15:30 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const a = employees.find((pessoa) => id === pessoa.id);
+  const bicho = a.responsibleFor[0];
+  let maior = 0;
+  let c;
+  species.find((anim) => anim.id === bicho).residents.forEach((item) => {
+    if (item.age > maior) {
+      maior = item.age;
+      c = item;
+    }
+  });
+  return [c.name, c.sex, c.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
