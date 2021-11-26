81f6f32110a2812ba07a400fc69e7dd9b750d60e
Vitor Campos <vitorcampos742@gmail.com>
2021-11-25 22:47:11 -0300

    requisitos 7 e 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,37 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+//  infelizmente tem que ficar com 2 funcSex pq o lint não aceita nem 3 if juntos na função
+function funcSex2(options, anim, aux) {
+  if (options.sex === 'female' && anim.sex === 'female') aux.push(anim.name);
+  if (options.sex === 'male' && anim.sex === 'male') aux.push(anim.name);
+}
+
+function funcSex(options, anim, aux) {
+  if (options.sex === undefined) aux.push(anim.name);
+  /* if (options.sex === 'female' && anim.sex === 'female') aux.push(anim.name)
+  if (options.sex === 'male' && anim.sex === 'male') aux.push(anim.name) */
+  funcSex2(options, anim, aux);
+}
+
+function getAnimalMap(
+  options = { includeNames: false, sorted: false, sex: undefined },
+) {
+  const a = {};
+  species.forEach((item) => {
+    if (!Object.keys(a).includes(item.location)) a[item.location] = [];
+    if (options.includeNames === true) {
+      const b = {};
+      const aux = [];
+      item.residents.forEach((anim) => {
+        funcSex(options, anim, aux);
+      });
+      if (options.sorted === true) aux.sort();
+      b[item.name] = aux;
+      a[item.location].push(b);
+    } else a[item.location].push(item.name);
+  });
+  return a;
 }
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,73 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function descobreParam(p1) {
+  const listaDays = Object.keys(hours);
+  const listaAnimals = species.map((item) => item.name);
+  if (listaAnimals.includes(p1)) return 'animal';
+  if (listaDays.includes(p1)) return 'dia';
+  return 'nada';
 }
+
+function tipoAnimal(p1) {
+  const b = species.find((item) => item.name === p1);
+  return b.availability;
+}
+
+function tipoDia(p1) {
+  const a = {};
+  const b = species.filter((bicho) => bicho.availability.includes(p1));
+  if (p1 === 'Monday') {
+    a[p1] = {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+    return a;
+  }
+  a[p1] = {
+    officeHour: `Open from ${hours[p1].open}am until ${hours[p1].close}pm`,
+    exhibition: b.map((item) => item.name),
+  };
+  return a;
+}
+
+/* const daymonday = {
+  'officeHour': 'CLOSED',
+  'exhibition': 'The zoo will be closed!',
+}; */
+const horas = Object.values(hours);
+function tipoNada() {
+  const a = {};
+  Object.keys(hours).forEach((dia, index) => {
+    if (dia === 'Monday') {
+      a[dia] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+      return a;
+    }
+    const b = species.filter((bicho) => bicho.availability.includes(dia));
+    a[dia] = {
+      officeHour: `Open from ${horas[index].open}am until ${horas[index].close}pm`,
+      exhibition: b.map((item) => item.name),
+    };
+  });
+  return a;
+}
+
+function getSchedule(scheduleTarget) {
+  const tipo = descobreParam(scheduleTarget);
+  if (tipo === 'nada') {
+    return tipoNada();
+  }
+  if (tipo === 'animal') {
+    return tipoAnimal(scheduleTarget);
+  }
+  if (tipo === 'dia') {
+    return tipoDia(scheduleTarget);
+  }
+}
+
+console.log(getSchedule('Monday'));
 
 module.exports = getSchedule;
